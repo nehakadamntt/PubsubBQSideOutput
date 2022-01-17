309e236d3e2ab79df3a63a6e6f1b7adc4a7084ef
nehakadamntt <96292026+nehakadamntt@users.noreply.github.com>
2022-01-17 21:08:46 +0530

    Update Main.java

--- GCP1/src/main/java/Main.java
+++ GCP1/src/main/java/Main.java
@@ -1,10 +1,11 @@
-
+import com.google.gson.Gson;
+import com.google.pubsub.v1.ProjectSubscriptionName;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 
 import org.apache.beam.runners.direct.DirectOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
-
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
@@ -12,14 +13,23 @@ import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
-
+import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.schemas.Schema;
 
-
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.*;
+import com.google.gson.JsonSyntaxException;
 
+import org.joda.time.Duration;
+import org.joda.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.beam.sdk.transforms.windowing.*;
+import org.apache.beam.sdk.values.*;
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableSchema;
+import java.util.Arrays;
 
 
 public class Main {
@@ -31,7 +41,10 @@ public class Main {
     static final TupleTag<String> unparsedMessages = new TupleTag<String>() {
     };
 
-
+    /*
+     * class provides the custom execution options passed by the
+     * executor at the command-line.
+     * */
     public interface MyOptions extends DataflowPipelineOptions, DirectOptions {
 
         @Description("BigQuery table name")
@@ -44,7 +57,7 @@ public class Main {
 
 
 
-
+        
 
     }
 
@@ -64,15 +77,22 @@ public class Main {
 
 
 
+
+
     public static final Schema rawSchema = Schema
             .builder()
-            .addInt32Field("id")
+            .addInt64Field("id")
             .addStringField("name")
             .addStringField("surname")
             .build();
+    
+        //     public static final TableSchema rawSchema = new TableSchema().setFields(Arrays.asList(
+        //         new TableFieldSchema().setName("id").setType("INTEGER"),
+        //         new TableFieldSchema().setName("name").setType("STRING"),
+        //         new TableFieldSchema().setName("surname").setType("STRING")
+        // ));
 
-
-    public static PipelineResult run(MyOptions options) throws Exception {
+    public static PipelineResult run(MyOptions options) throws Exception{
         Pipeline pipeline = Pipeline.create(options);
 
         LOG.info("Building pipeline...");
@@ -83,11 +103,25 @@ public class Main {
         PCollection<Account> accountCollection=transformOut.get(PubsubMessageToAccount.parsedMessages);
         PCollection<String> unparsedCollection=transformOut.get(PubsubMessageToAccount.unparsedMessages);
 
-        accountCollection.apply("WriteToBigquery", BigQueryIO.<Account>write().to(options.getOutputTableName()).useBeamSchema()
-                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
-                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+        accountCollection.apply(ParDo.of(new DoFn<Account,String>() {
+            @ProcessElement
+            public void processElement( ProcessContext context)
+            {
+                Gson g=new Gson();
+                String s=g.toJson(context.element());
+                 context.output(s);
+            }
 
-        unparsedCollection.apply("write to dlq",PubsubIO.writeStrings().to("projects/nttdata-c4e-bde/topics/uc1-dlq-topic-7"));
+        })).apply(JsonToRow.withSchema(rawSchema)).apply("WriteToBigquery", BigQueryIO.<Row>write().
+                to(options.getOutputTableName()).useBeamSchema()
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+
+
+        
+       
+
+        unparsedCollection.apply("write to dlq",PubsubIO.writeStrings().to("path_to_dlq"));
 
 
         return pipeline.run();
