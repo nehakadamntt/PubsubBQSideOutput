9fb36dadbf0b2e06471fe945be3cc2fe069fb444
nehakadamntt <96292026+nehakadamntt@users.noreply.github.com>
2022-01-17 21:05:45 +0530

    Update Account.java

--- GCP1/src/main/java/Account.java
+++ GCP1/src/main/java/Account.java
@@ -1,16 +1,16 @@
 import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+import org.apache.beam.sdk.schemas.annotations.SchemaCreate;
 @DefaultSchema(JavaFieldSchema.class)
 public class Account {
-    private int id;
+    private  int id;
     private  String name;
     private  String surname;
-    @Override
-    public String toString() {
-        return "{" +
-                "\nid:" + this.id +
-                "\nname:" + this.name +
-                "\nsurname:" + this.surname +
-                "\n}";
+    
+    @SchemaCreate
+    public Account(int id,String name,String surname) {
+     this.id=id;
+     this.name=name;
+     this.surname=surname;
     }
 }
