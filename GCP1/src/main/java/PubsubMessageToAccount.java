a84a3904a5a0c451faa9685dd135f7f9f61d7d05
nehakadamntt <96292026+nehakadamntt@users.noreply.github.com>
2022-01-18 10:26:51 +0530

    comments added

--- GCP1/src/main/java/PubsubMessageToAccount.java
+++ GCP1/src/main/java/PubsubMessageToAccount.java
@@ -9,13 +9,14 @@ import org.apache.beam.sdk.values.TupleTagList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
+//converting jsonString to Account object and also differentiate the valid an invalid data
+//returns tupletagPCollection with 2 tupletags first is parsedMessage for correct data and unparsedMassage for incorrect data
 
 public  class PubsubMessageToAccount extends DoFn<String, Account> {
      
 
-    public static TupleTag<Account> parsedMessages = new TupleTag<Account>(){};
-    public static TupleTag<String> unparsedMessages = new TupleTag<String>(){};
+    public static TupleTag<Account> parsedMessages = new TupleTag<Account>(){};  //for correct data
+    public static TupleTag<String> unparsedMessages = new TupleTag<String>(){};  //for incorrect data
 
     public static PCollectionTuple expand(PCollection<String> input) throws Exception {
         return input
@@ -24,11 +25,11 @@ public  class PubsubMessageToAccount extends DoFn<String, Account> {
                             public void processElement(@Element String s ,ProcessContext context) {
                                 try {
 
-                                    Gson gson = new Gson();
-                                   Account a=gson.fromJson(s,Account.class);
-                                    context.output(parsedMessages, a);
+                                    Gson gson = new Gson();  //craete Gson object
+                                   Account a=gson.fromJson(s,Account.class); //convert jsonString Messsage to Account class
+                                    context.output(parsedMessages, a); 
                                 } catch (Exception e) {
-                                    e.printStackTrace();
+                                    e.printStackTrace();  //exeception occure if data is inavlid
                                     context.output(unparsedMessages,s);
                                 }
 
